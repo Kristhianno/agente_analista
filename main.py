4bb9972ea24ddd78871dbf40b2726f3e33b7eabc
Efraim Kristhianno <ekriator@gmail.com>
2025-02-27 09:58:00 -0300

    att

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from projetoaileitor!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ programa.py
@@ -0,0 +1,42 @@
+import os
+from crewai import Agent, Task, Crew, Process
+from crewai_tools import FileReadTool
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+load_dotenv()
+
+tool = FileReadTool()
+llm = ChatGroq(model='llama3-70b-8192')
+api_key = os.environ.get("GROQ_API_KEY")
+
+
+agent = Agent(
+    role='Analista de Documentos',
+    goal='Ler o arquivo {input} e trazer detalhes do documento.',
+    backstory='Você é um experiente analista de documentos capaz de trazer detalhes que poucas pessoas observariam.',
+    tools=[tool],
+    verbose=True,
+    allow_delegation=False,
+    llm=llm
+)
+
+task = Task(
+    description='Sua tarefa é executar uma análise detalhada do documento {input}',
+    expected_output='Uma análise detalhada do documento {input}.',
+    tools=[tool],
+    agent=agent
+)
+
+crew = Crew(
+    agents=[agent],
+    tasks=[task],
+    verbose=True,
+    process=Process.sequential
+)
+
+while True:
+    input = input('O arquivo é : ')
+    if input == 'exit':
+        break
+    result = crew.kickoff(inputs={"input": input})
+    print(result)
