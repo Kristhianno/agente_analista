54a103903bcaac6f9cfe6e87dbb335841575ba9a
Efraim Kristhianno <ekriator@gmail.com>
2025-02-27 15:15:58 -0300

    att

--- programa.py
+++ programa.py
@@ -1,53 +1,50 @@
 import os
-import streamlit as st
 from crewai import Agent, Task, Crew, Process
-from crewai_tools import PDFSearchTool
+from crewai_tools import DallETool
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
 
-def main():
-
-    st.title('Agente Analisador')
-    
-    arquivo = st.file_uploader(label='Coloque o arquivo aqui.')
-
-    botao = st.button(label='iniciar análise')
-
-    llm = ChatOpenAI(model='openai/gpt-4o-mini')
-    api_key = os.environ.get("OPENAI_API_KEY")
+llm = ChatOpenAI(model='openai/gpt-4o-mini')
+api_key = os.environ.get("OPENAI_API_KEY")
 
 
-    tool = PDFSearchTool()
+tool = DallETool(
+    model='dall-e-3',
+    size= '1024x1024',
+    quality='hd',
+    n=1
+)
 
-    agent = Agent(
-        role='Analista de Documentos',
-        goal='Ler o arquivo {arquivo} e trazer detalhes do documento.',
-        backstory='Você é um experiente analista de documentos capaz de trazer detalhes que poucas pessoas observariam.',
-        tools=[tool],
-        verbose=True,
-        allow_delegation=False,
-        llm=llm
-    )
+agent = Agent(
+    role='Criador de imagens',
+    goal='Ler o arquivo {arquivo} e fazer uma imagem realista e precisa.',
+    backstory='Você é um experiente designer e ilustrador  capaz de trazer detalhes que poucas pessoas observariam.',
+    tools=[tool],
+    verbose=True,
+    allow_delegation=False,
+    llm=llm
+)
 
-    task = Task(
-        description='Sua tarefa é executar uma análise detalhada do documento {arquivo} e trazer em tópicos o que você viu',
-        expected_output='Uma análise detalhada do documento {arquivo} sugerindo melhorias.',
-        tools=[tool],
-        agent=agent
-    )
+task = Task(
+    description='Sua tarefa é criar uma imagem realista com riquezas de detalhes do {arquivo} e trazer uma imagem impactante',
+    expected_output='Uma análise detalhada do documento {arquivo} trazendo uma imagem realista.',
+    tools=[tool],
+    agent=agent
+)
 
-    crew = Crew(
-        agents=[agent],
-        tasks=[task],
-        verbose=True,
-        process=Process.sequential
-    )
+crew = Crew(
+    agents=[agent],
+    tasks=[task],
+    verbose=True,
+    process=Process.sequential
+)
 
-    result = botao(crew.kickoff(inputs={"input": arquivo}))
-        
+arquivo = input('Qual é o prompt : ')
+if arquivo == True:
+    result = crew.kickoff(inputs={"input": arquivo})
+    FileNotFoundError('por favor coloque um arquivo')
     print(result)
 
 
-main()
